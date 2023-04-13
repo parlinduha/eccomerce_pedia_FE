09d4f3fbe3db52b4c09648bef8990313eda6e072
ParlinD <duhaperi@gmail.com>
2023-04-13 15:56:50 +0700

    journey upload project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eccomerce_pediaApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
